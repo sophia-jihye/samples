b8c70bb0406805abe25f2a337ebcb34f0ca23cd8
jihye <jihye@13GRAM-i5-JIHYE>
2017-12-17 21:51:21 +0900

    first commit

---
+++ src/samples/liveUpdate/ShellControl.java
@@ -0,0 +1,28 @@
+package samples.liveUpdate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ShellControl {
+
+	public void shellCmd(String command) {
+		Runtime runtime = Runtime.getRuntime();
+		Process process = null;
+
+		try {
+			process = runtime.exec(command);
+			InputStream is = process.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/samples/liveUpdate/VersionDto.java
@@ -0,0 +1,27 @@
+package samples.liveUpdate;
+
+public class VersionDto {
+
+	private String version;
+
+	public VersionDto() {
+	}
+
+	public VersionDto(String version) {
+		this.version = version;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	@Override
+	public String toString() {
+		return "VersionDto [version=" + version + "]";
+	}
+
+}
