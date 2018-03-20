fcf5507d637e3eec3d463d75c78b16c8f327d40c
Jihye Park <jihyeparkk@naver.com>
2018-03-20 11:15:40 +0900

    added lib

--- src/samples/simpleExcel/Main.java
+++ src/samples/simpleExcel/Main.java
@@ -13,14 +13,14 @@ public class Main {
 		List<CustomerVo> xlsList = excelReader
 				.xlsCustomerVoList("C:\\DevelopTools\\Eclipse\\Workspace\\PRIVATE\\samples\\input\\1.xls");
 		System.out.println(xlsList);
-		System.out.println("======��======");
+		System.out.println("======xls read end======");
 
 		System.out.println("===xlsx===");
 		List<CustomerVo> xlsxList = excelReader
 				.xlsxCustomerVoList("C:\\DevelopTools\\Eclipse\\Workspace\\PRIVATE\\samples\\input\\2.xlsx");
 		System.out.println(xlsxList);
-		System.out.println("======��======");
-		System.out.println("* Reader ��* ");
+		System.out.println("======xlsx read end======");
+		System.out.println("* Reader End* ");
 
 		System.out.println("* Writer *");
 		CustomerExcelWriter excelWriter = new CustomerExcelWriter();
@@ -33,13 +33,13 @@ public class Main {
 		excelWriter
 				.xlsWriter(list,
 						"C:\\DevelopTools\\Eclipse\\Workspace\\PRIVATE\\samples\\output\\1.xls");
-		System.out.println("======��======");
+		System.out.println("======xls write end======");
 
 		System.out.println("===xlsx===");
 		excelWriter
 				.xlsxWriter(list,
 						"C:\\DevelopTools\\Eclipse\\Workspace\\PRIVATE\\samples\\output\\2.xls");
-		System.out.println("======��======");
-
+		System.out.println("======xlsx write end======");
+		System.out.println("* Writer End* ");
 	}
 }
